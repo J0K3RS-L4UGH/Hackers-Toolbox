f2c71518e693b8828cb70c325f0b19d282b92065
J0K3RS-L4UGH <95757914+J0K3RS-L4UGH@users.noreply.github.com>
2022-07-16 18:37:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
-sudo apt-get update
-
+sudo apt-get update -y
 sudo apt-get install whois -y
+sudo apt-get update -y
 sudo apt-get install sqlmap -y
+sudo apt-get update -y
 sudo apt-get install netcat -y
+sudo apt-get update -y
 sudo apt-get install nmap -y
+sudo apt-get update -y
 sudo apt-get install hashcat -y
+sudo apt-get update -y
 sudo apt-get install crunch -y
+sudo apt-get update -y
 sudo apt-get install cupp -y
+sudo apt-get update -y
 sudo apt-get install john -y
+sudo apt-get update -y
 sudo apt-get install hydra -y
 
 mkdir .sherlock
