66b1c3a0c7bdbd61b83a54aa4afc2e5c246350c9
J0K3RS-L4UGH <95757914+J0K3RS-L4UGH@users.noreply.github.com>
2022-08-21 12:50:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,53 @@
-sudo apt-get update -y && sudo apt-get upgrade -y
+#!bin/bash
 
-sudo apt-get install whois -y
+yes="y"
 
-sudo apt-get install sqlmap -y
+echo -e "Terms of sevice:\n1. The creator of this tool can not be held liable for any of your actions involving this script\n2. No illegal actions will be commited with this script\nDo you accept these Terms and Conditions? [y/n]:"
+read tnc
 
-sudo apt-get install netcat -y
-
-sudo apt-get install nmap -y
-
-sudo apt-get install hashcat -y
-
-sudo apt-get install crunch -y
-
-sudo apt-get install cupp -y
-
-sudo apt-get install john -y
-
-sudo apt-get install hydra -y
-
-sudo apt-get install wifite -y
-
-sudo mkdir .sherlock
-cd .sherlock
-sudo git clone https://github.com/sherlock-project/sherlock.git
-cd sherlock
-sudo pip3 install -r requirements.txt
-
-cd ..
-
-y="y"
-n="n"
-echo "Would you like to run Hackers Toolbox (python3 HackersToolbox.py)? [y/n]:"
-read yn
-if [ $yn == $y ]
+if [ $tnc = $yes ]
 then
- python3 HackersToolbox.py
+  sudo apt-get update -y && sudo apt-get upgrade -y
+
+  sudo apt-get install whois -y
+
+  sudo apt-get install sqlmap -y
+
+  sudo apt-get install netcat -y
+
+  sudo apt-get install nmap -y
+
+  sudo apt-get install hashcat -y
+
+  sudo apt-get install crunch -y
+
+  sudo apt-get install cupp -y
+
+  sudo apt-get install john -y
+
+  sudo apt-get install hydra -y
+
+  sudo apt-get install wifite -y
+
+  sudo mkdir .sherlock
+  cd .sherlock
+  sudo git clone https://github.com/sherlock-project/sherlock.git
+  cd sherlock
+  sudo pip3 install -r requirements.txt
+
+  cd ..
+
+  y="y"
+  n="n"
+  echo "Would you like to run Hackers Toolbox (python3 HackersToolbox.py)? [y/n]:"
+  read yn
+  if [ $yn == $y ]
+  then
+   python3 HackersToolbox.py
+  else
+    echo 'Thanks for installing!'
+  fi
 else
-  echo 'Thanks for installing!'
+  cd ..
+  rm -r Hackers-Toolbox
 fi
